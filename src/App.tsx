3bb4284784f10cbcd307600c59f0c044aa7de892
Najeem Kanishka <contact@najeemkanishka.com>
2021-09-01 01:37:10 -0400

    Created home; Moved out of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,6 @@
 import React from "react";
-import logo from "./media/logo.svg";
-import "./styles/App.css";
-
 function App() {
-  return (
-    <div className="App text-center">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>React with Tailwind CSS</p>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import logo from "./media/logo.svg";
+import "./styles/App.css";
+
+export const Home = () => {
+  return (
+    <>
+      <div className="App text-center">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>React with Tailwind CSS</p>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
+  );
+};

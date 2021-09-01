6c75b5a6c5f349bcc80520cd4af14efd4cf2bd5c
Najeem Kanishka <contact@najeemkanishka.com>
2021-08-31 23:20:38 -0400

    Installed Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './media/logo.svg';
-import './styles/App.css';
+import React from "react";
+import logo from "./media/logo.svg";
+import "./styles/App.css";
 
 function App() {
   return (
